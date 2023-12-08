98f7b6d61cdbf85f763de1c58892d9a4819bc713
Igoris Ivanovas <igoris.x@inbox.lt>
2023-12-08 18:27:48 +0100

    Header setup

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 https://igoris12.github.io/pomodoro-app/
 
 start 7/12/2023
+
+* components style is in the same folder.
+* Components class has the same name as the component starting with lowercase.
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './Header.scss';
 
 function Header() {
   return (
-    <div>
+    <header className='header'>
       Header 
-    </div>
+    </header>
   )
 }
 
