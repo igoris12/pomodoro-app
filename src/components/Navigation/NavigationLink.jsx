a2046aa57eb33f96cc41137c263dd669132ea1bd
Igoris Ivanovas <igoris.x@inbox.lt>
2023-12-11 19:40:47 +0100

    working on navLink active

--- src/components/Navigation/NavigationLink.jsx
+++ src/components/Navigation/NavigationLink.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./NavigationLink.scss";
 import { Link } from "react-router-dom";
 
-function NavigationLink({ icon, text, to, darkMode = false, def = false }) {
+function NavigationLink({ icon, text, to, darkMode = false}) {
   return (
     <Link
       to={to}
       className={
         !true === darkMode
-          ? `navigationLink ${true === def ? "active" : ""}`
-          : `navigationLink dark ${true === def ? 'active': ''}`
+          ? `navigationLink }`
+          : `navigationLink dark }`
       }
     >
       {icon} {text}
--- src/components/Navigation/js/activeLink.js
+++
@@ -1,7 +0,0 @@
-const activeLink = (e, className) => {
-    console.log(e.target);
-    const navDOM = document.getElementsByClassName(className);
-    console.log(navDOM[0]);
-}
-
-export default activeLink
