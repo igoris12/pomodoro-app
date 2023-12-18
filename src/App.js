46abcd0658316999d6644e212f47f2595b754d3b
Igoris Ivanovas <igoris.x@inbox.lt>
2023-12-18 17:54:39 +0100

    Timer setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Timer from "./components/Timer/Timer";
 function App() {
   const [mobileHeaderActive, setMobileHeaderActive] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
@@ -24,7 +25,7 @@ function App() {
           darkMode={darkMode}
         />
         <Routes>
-        <Route path="/" element={<main>main</main>} />
+        <Route path="/" element={<Timer/>} />
         <Route path="/settings" element={<main>settings</main>} />
         </Routes>
       </Router>
---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Timer.scss';
+function Timer() {
+  return (
+    <mian className='timerContainer'>
+      Timer
+    </mian>
+  )
+}
+
+export default Timer
