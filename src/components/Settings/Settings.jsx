10843bb95f315236f938430897f68ff7cbb331ae
Igoris Ivanovas <igoris.x@inbox.lt>
2023-12-31 10:55:49 +0200

    Footer set to Settings

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import './Settings.scss';
+import "./Settings.scss";
+import Footer from '../Footer/Footer';
 
 const Settings = ({ darkMode }) => {
-  return <section className="settings">Settings 1</section>;
+  return <section className="settings">Settings 1
+  <Footer darkMode={darkMode}/></section>;
 };
 
 export default Settings;
