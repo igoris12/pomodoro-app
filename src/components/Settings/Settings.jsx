5aaf49f85dee431e2838e8ab7097750f68e0b9e8
Igoris Ivanovas <igoris.x@inbox.lt>
2023-12-31 10:52:53 +0200

    Setting setup

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Settings.scss';
+
+const Settings = ({ darkMode }) => {
+  return <section className="settings">Settings 1</section>;
+};
+
+export default Settings;
