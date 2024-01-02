e345fdd87a59dfe9bc2a36dd2abfe45e63b7bee8
Igoris Ivanovas <igoris.x@inbox.lt>
2024-01-02 17:32:45 +0200

    RangeInpput update

--- src/components/Settings/RangeInput.jsx
+++ src/components/Settings/RangeInput.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import './RangeInput.scss';
-function RangeInput({value, max, min, name, text}) {
+function RangeInput({value, max, min, name, text, defValue}) {
   return (
     <div className="rangeInput">
-      <input type="range" id="volume" name="volume" min="0" max="11"  value="10"/>
-      <label for="volume">Volume</label>
+      <label htmlFor={name}>{text}</label>
+      <input type="range"  name={name} min={min} max={max}  value={value}/>
     </div>
   );
 }
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -7,10 +7,20 @@ const Settings = ({ darkMode }) => {
   return (
     <section className={darkMode !== true ? "settings" : "settings dark"}>
       <form>
-        <RangeInput />
-        <RangeInput />
-        <RangeInput />
-        <RangeInput />
+        <RangeInput text={"Work duration"} min={5} max={60} defValue={25} />
+        <RangeInput
+          text={"Short break duration"}
+          min={1}
+          max={30}
+          defValue={5}
+        />
+        <RangeInput
+          text={"Long break duration"}
+          min={1}
+          max={45}
+          defValue={20}
+        />
+        <RangeInput text={"Rounds"} min={2} max={15} defValue={4} />
       </form>
       <Footer darkMode={darkMode} />
     </section>
