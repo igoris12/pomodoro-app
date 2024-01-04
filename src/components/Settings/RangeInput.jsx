06bb97f8c10039572838027d900d93a342724468
Igoris Ivanovas <igoris.x@inbox.lt>
2024-01-04 19:25:01 +0200

    onChange for RangeInput

--- src/components/Settings/RangeInput.jsx
+++ src/components/Settings/RangeInput.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './RangeInput.scss';
-function RangeInput({value, max, min, name, text, defValue}) {
+function RangeInput({value, max, min, name, text, defValue, change}) {
   return (
     <div className="rangeInput">
       <label htmlFor={name}>{text}</label>
-      <input type="range"  name={name} min={min} max={max}  value={value}/>
+      <samp className="value">{value}</samp>
+      <input type="range"  name={name} min={min} max={max}  value={value} onChange={change}/>
     </div>
   );
 }
