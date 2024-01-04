2bdf630fdbfdc7e0b4305d3660a7059cdfbf321d
Igoris Ivanovas <igoris.x@inbox.lt>
2024-01-04 19:39:42 +0200

    value in place

--- src/components/Settings/RangeInput.jsx
+++ src/components/Settings/RangeInput.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import './RangeInput.scss';
-function RangeInput({value, max, min, name, text, defValue, change}) {
+import "./RangeInput.scss";
+function RangeInput({ value, max, min, name, text, defValue, change }) {
   return (
     <div className="rangeInput">
       <label htmlFor={name}>{text}</label>
-      <samp className="value">{value}</samp>
-      <input type="range"  name={name} min={min} max={max}  value={value} onChange={change}/>
+      <span
+        className="value"
+        style={{ left:  value  / max  * 100 - min  / max  * 100 + '%' }}
+      
+      >
+        {value}
+      </span>
+      <input
+        type="range"
+        name={name}
+        min={min}
+        max={max}
+        value={value}
+        onChange={change}
+      />
+      {/* <span className="min">{min}</span>
+      <span className="max">{max}</span> */}
     </div>
   );
 }
