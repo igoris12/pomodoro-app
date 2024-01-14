5c62fede3d23dcd3c2c4b89ac77d69b591f45e6b
Igoris Ivanovas <igoris.x@inbox.lt>
2024-01-14 17:30:55 +0100

    add useContext for state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Timer from "./components/Timer/Timer";
 import Settings from "./components/Settings/Settings";
+
+export const TimerDataContext = createContext();
+
 function App() {
   const [mobileHeaderActive, setMobileHeaderActive] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
-
+  const [timerData, setData] = useState([
+    { time: 90, status: "focus 1", session: 1 },
+    { time: 5, status: "brack", session: 2 },
+    { time: 90, status: "focus 2", session: 3 },
+    { time: 5, status: "brack", session: 2 },
+    { time: 90, status: "focus 3", session: 1 },
+    { time: 5, status: "brack", session: 2 },
+    { time: 90, status: "focus 4", session: 1 },
+    { time: 100, status: "big brake", session: 5 },
+  ]);
 
   const toggleMobileHeaderActive = () => {
     setMobileHeaderActive(!mobileHeaderActive);
@@ -17,6 +29,7 @@ function App() {
   }
   return (
     <div className="App">
+      <TimerDataContext.Provider value={timerData}>
       <Router>
         <Header openNavigation={toggleMobileHeaderActive} darkMode={darkMode} />
         <Navigation
@@ -30,6 +43,7 @@ function App() {
         <Route path="/pomodoro-app/settings" element={<Settings darkMode={darkMode}/>} />
         </Routes>
       </Router>
+      </TimerDataContext.Provider>
     </div>
   );
 }
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,20 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import {TimerDataContext} from '../../App';
 import "./Timer.scss";
 import TimerProgressBar from "./TimerProgressBar";
 import Footer from "../Footer/Footer";
-function Timer({ darkMode }) {
-  const [data, setData] = useState([
-    { time: 90, status: "focus 1", session: 1 },
-    { time: 5, status: "brack", session: 2 },
-    { time: 90, status: "focus 2", session: 3 },
-    { time: 5, status: "brack", session: 2 },
-    { time: 90, status: "focus 3", session: 1 },
-    { time: 5, status: "brack", session: 2 },
-    { time: 90, status: "focus 4", session: 1 },
-    { time: 100, status: "big brake", session: 5 },
-  ]);
+function Timer({ darkMode}) {
+  const data = useContext(TimerDataContext);
   const [session, setSession] = useState(1);
   const [time, setTime] = useState(data[session - 1].time);
+
 
   const changeSession = () => {
     if (session === data.length) {
