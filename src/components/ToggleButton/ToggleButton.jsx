7461617a1f49204a7a7fc97684d839e9b7ecf926
Igoris Ivanovas <igoris.x@inbox.lt>
2024-01-22 20:42:12 +0100

    simantic

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ToggleButton.scss";
 const ToggleButton = () => {
-  return <div className="ToggleButton">button</div>;
+  return <button className="ToggleButton">button</button>;
 };
 
 export default ToggleButton;
