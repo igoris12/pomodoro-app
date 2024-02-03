0fa98cd712ea8baadd8652344617399cd142910b
Igoris Ivanovas <igoris.x@inbox.lt>
2024-02-03 15:28:22 +0100

    update

--- src/components/SelectInput/SelectInput.jsx
+++ src/components/SelectInput/SelectInput.jsx
@@ -4,11 +4,11 @@ import './SelectInput.scss';
 function SelectInput() {
   return (
     <>
-      <label for="sounds" className="selectInput">
+      <label htmlFor="sounds" className="selectInput">
         <span>Notificaiton sound</span>
         <select name="sounds">
           <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
+          <option value="saab" selected>Saab</option>
           <option value="mercedes">Mercedes</option>
           <option value="audi">Audi</option>
         </select>
