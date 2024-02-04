8e38997920224ad4e7ad9f1fb31db0f3cb6fae89
Igoris Ivanovas <igoris.x@inbox.lt>
2024-02-04 15:31:27 +0100

    from to sass done

--- src/components/SelectInput/CustomSelectInput.jsx
+++ src/components/SelectInput/CustomSelectInput.jsx
@@ -1,36 +1,41 @@
-import React, {useState} from "react";
-import './CustomSelectInput.scss';
+import React, { useState } from "react";
+import "./CustomSelectInput.scss";
 // import './CustomSelectInput.js'
 
-
 // selectBtn.addEventListener("click", () => {
-  //   // add/remove active class on the container element
-  //   customSelect.classList.toggle("active");
-  //   // update the aria-expanded attribute based on the current state
-  //   selectBtn.setAttribute(
+//   // add/remove active class on the container element
+//   customSelect.classList.toggle("active");
+//   // update the aria-expanded attribute based on the current state
+//   selectBtn.setAttribute(
 //     "aria-expanded",
 //     selectBtn.getAttribute("aria-expanded") === "true" ? "false" : "true"
 //   );
 // });
 function CustomSelectInput() {
   const [customSelectActive, setCustomSelectActive] = useState(false);
-  const [selected, setSelected] = useState(1)
+  const [selected, setSelected] = useState("soud1");
 
-  const toggleCustomSelectActive = ()=> {
+  const toggleCustomSelectActive = () => {
     setCustomSelectActive(!customSelectActive);
-  }
+  };
+
+  const options = [
+    { name: "soud1", id: 0 },
+    { name: "soud2", id: 1 },
+  ];
 
   return (
-    <div className={customSelectActive ?  "custom-select active": 'custom-select'}>
-     {/* <select></select> */}
+    <div
+      className={customSelectActive ? "custom-select active" : "custom-select"}
+    >
       <button
         className="select-button"
         role="combobox"
         aria-labelledby="select button"
         aria-haspopup="listbox"
-        aria-expanded={customSelectActive ? "true" : 'fasle'}
+        aria-expanded={customSelectActive ? "true" : "fasle"}
         aria-controls="select-dropdown"
-        onClick={(e)=> {
+        onClick={(e) => {
           e.preventDefault();
           toggleCustomSelectActive();
         }}
@@ -39,43 +44,39 @@ function CustomSelectInput() {
         <span className="arrow"></span>
       </button>
       <ul className="select-dropdown" role="listbox" id="select-dropdown">
-        <li role="option" aria-selected={true} >
-          <input type="radio" id="github" name="social-account" />
+        {options.map((item) => {
+          return (
+            <li
+              onClick={(e) => {
+                setSelected(item.name);
+              }}
+              key={item.id}
+              role="option"
+              aria-selected
+              className={selected === item.name ? "active" : ""}
+            >
+              <label>
+                <input type="radio" name="soud" />
+                <i className="bx bxl-github"></i>
+                {item.name}
+              </label>
+            </li>
+          );
+        })}
+
+        {/* <li role="option">
           <label htmlFor="github">
+            <input type="radio" name="social-account" />
             <i className="bx bxl-github"></i>GitHub
           </label>
         </li>
-        <li role="option" aria-selected={false} >
-          <input type="radio" id="github" name="social-account" />
+
+        <li role="option">
           <label htmlFor="github">
+            <input type="radio" name="social-account" />
             <i className="bx bxl-github"></i>GitHub
           </label>
-        </li> <li role="option"  >
-          <label htmlFor="github">
-          <input type="radio"  id="github" name="social-account" />
-            <i className="bx bxl-github"></i>GitHub
-          </label>
-        </li> <li role="option" aria-selected={false} >
-          <label htmlFor="github">
-          <input type="radio" id="github" name="social-account" />
-            <i className="bx bxl-github"></i>GitHub
-          </label>
-        </li> <li role="option" aria-selected={false} >
-          <label htmlFor="github">
-          <input type="radio" id="github" name="social-account" />
-            <i className="bx bxl-github"></i>GitHub
-          </label>
-        </li> <li role="option" aria-selected={false} >
-          <label htmlFor="github">
-          <input type="radio" id="github" name="social-account" />
-            <i className="bx bxl-github"></i>GitHub
-          </label>
-        </li> <li role="option" aria-selected={false} >
-          <label htmlFor="github">
-          <input type="radio" id="github" name="social-account" />
-            <i className="bx bxl-github"></i>GitHub
-          </label>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
