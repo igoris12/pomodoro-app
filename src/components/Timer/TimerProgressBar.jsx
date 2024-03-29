d0cd91809153443732c9e8cd11c3f552cdb9f1a3
Igoris Ivanovas <igoris.x@inbox.lt>
2024-03-29 19:25:07 +0100

    find new audio bug)

--- src/components/Timer/TimerProgressBar.jsx
+++ src/components/Timer/TimerProgressBar.jsx
@@ -42,15 +42,12 @@ function TimerProgressBar({ darkMode }) {
   const data = formatData(reduxData);
   const [session, setSession] = useState(1);
   const [time, setTime] = useState(data.time[session - 1].time);
-
-  console.log(data.sound.name);
-  console.log(data.time[0]);
-
   const [dinamicTime, setDinamincTime] = useState(time);
 
   const minutes = Math.floor(dinamicTime / 60);
   const seconds = dinamicTime - minutes * 60;
 
+  const sound = new Audio(data.sound.audio);
 
   const reduceTime = () => {
     setDinamincTime((prevTime) => prevTime - 1);
@@ -58,11 +55,10 @@ function TimerProgressBar({ darkMode }) {
   useEffect(()=> {
     setTime(data.time[session - 1].time);
   },[session, data])
+
   useEffect(() => {
     if (dinamicTime === 0) {
-      const sound = new Audio(data.sound.audio);
       sound.play();
-
       if (data.settings.notification) {
         callNotification();
       }
@@ -122,6 +118,8 @@ function TimerProgressBar({ darkMode }) {
   };
 
   const togglePlay = () => {
+    sound.pause();
+
     setPlay(!play);
   };
 
